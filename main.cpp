f16697897dad29c7983dddc8a8e2b67c6204f95e
Kamaev Anton <alk@m2lab.ru>
2026-09-03 16:00:09 +0300

    Фиксы

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#include <array>
+#include <cstddef>
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "include/cpp_test/chat.h"
@@ -9,53 +12,53 @@
 // #include <userver/server/handlers/tests_control.hpp>
 // #include <userver/utils/daemon_run.hpp>
 
-#include <array>
-#include <memory>
-
 #include "hello_handler.hpp"
 
-constexpr std::array<std::string, 3> names = {"Anton", "Dima", "Masha"};
-constexpr std::array<std::string, 3> emails = {"anton@anton.ru", "dima@dima.ru", "masha@masha.ru"};
+namespace {
 
-static void generateUser(User &u, const int index)
-{
-    u.setName(names[index]);
-    u.setEmail(emails[index]);
-    u.setUsername(names[index]);
+    const std::array<std::string, 3> names = {"Anton", "Dima", "Masha"};
+    const std::array<std::string, 3> emails = {"anton@anton.ru", "dima@dima.ru", "masha@masha.ru"};
+
+    void generateUser(User &u, const std::size_t index)
+    {
+        u.setName(names[index]);
+        u.setEmail(emails[index]);
+        u.setUsername(names[index]);
+    }
+
 }
 
-int main(int argc, char *argv[]) {
-    std::array<std::unique_ptr<User>, names.size()> users;
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+{
+    std::array<User, names.size()> users;
 
-    for (int i = 0; i < names.size(); ++i)
+    for (std::size_t i = 0; i < names.size(); ++i)
     {
-        users[i] = std::make_unique<User>();
-        generateUser(*users[i], i);
+        generateUser(users[i], i);
     }
 
     try {
         std::vector<Chat> chats;
-        Chat c("Chat1"), c2("Chat2");
+        chats.reserve(2);
+        chats.emplace_back("Chat1");
+        chats.emplace_back("Chat2");
 
-        c.addUser(*users[0]);
-        c.addUser(*users[2]);
-        c.addMessage("Привет, как дела?", STRING, *users[0]);
+        chats[0].addUser(users[0]);
+        chats[0].addUser(users[2]);
+        chats[0].addMessage("Привет, как дела?", STRING, users[0]);
 
-        c2.addUser(*users[1]);
-        c2.addUser(*users[0]);
-        c2.addMessage("Димка, в майн пойдешь?", STRING, *users[0]);
+        chats[1].addUser(users[1]);
+        chats[1].addUser(users[0]);
+        chats[1].addMessage("Димка, в майн пойдешь?", STRING, users[0]);
 
-        chats.push_back(c);
-        chats.push_back(c2);
-
-        for (auto& chat : chats)
+        for (auto &chat : chats)
         {
             chat.getInfo();
         }
-    } catch (std::exception &e) {
-        std::cerr << e.what();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
     }
-    //
+
     // auto component_list = userver::components::MinimalServerComponentList()
     //                               .Append<userver::clients::dns::Component>()
     //                               .Append<HelloHandler>();
